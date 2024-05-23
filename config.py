fce681ff617ce9cd4fc1ddec26bd07a27abd5499
MatheusMedeiros <mths.medeirosa@gmail.com>
2024-05-23 01:21:04 -0300

    config de banco

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'you-will-never-guess')
-    MYSQL_HOST = 'localhost'
+    MYSQL_HOST = 'monorail.proxy.rlwy.net'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = '1234'
-    MYSQL_DB = 'graduation_gifts'
+    MYSQL_PASSWORD = 'GdPylWDoCophtDEYuHBQJHoqoIvzQSJY'
+    MYSQL_DB = 'railway'
     # Caminho absoluto para a pasta de uploads dentro de static
     UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app', 'static', 'uploads')
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
